8e05941a46f2667c99caf1b92d17206576093092
Aryaman <34205435+absterjr@users.noreply.github.com>
2023-02-21 13:15:49 +0530

    Update Q2.c

--- VIth Sem/Parallel Programming/Week 2/Q2.c
+++ VIth Sem/Parallel Programming/Week 2/Q2.c
@@ -2,15 +2,14 @@
 #include <omp.h>
 #include <math.h>
 int main() {
-
+	int i;
+	printf("Enter an integer value of i: ");
+	scanf_s("%d", &i);
 #pragma omp parallel
-{
-		int i;
-		printf("Enter an integer value of i: ");
-		scanf_s("%d", &i);
-   int x = omp_get_thread_num();
-   double result = pow(i, x);
-   printf("pow(%d, %d) = %lf (thread %d)\n", i, x, result, x);
-}
-return 0;
+	{
+		int x = omp_get_thread_num();
+		double result = pow(i, x);
+		printf("pow(%d, %d) = %lf (thread %d)\n", i, x, result, x);
+	}
+	return 0;
 }
