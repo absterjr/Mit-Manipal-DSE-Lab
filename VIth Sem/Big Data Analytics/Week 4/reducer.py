700212f9ec10b55780856bd0a66b154d0761ba22
Aryaman <34205435+absterjr@users.noreply.github.com>
2023-05-10 17:48:05 +0530

    reducer.py

---
+++ VIth Sem/Big Data Analytics/Week 4/reducer.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+import sys
+
+current_word = None
+current_count = 0
+
+# input comes from STDIN (standard input)
+for line in sys.stdin:
+    # remove leading and trailing whitespace
+    line = line.strip()
+
+    # parse the input we got from mapper.py
+    word, count = line.split('\t', 1)
+
+    # convert count (currently a string) to int
+    try:
+        count = int(count)
+    except ValueError:
+        # count was not a number, so silently
+        # ignore/discard this line
+        continue
+
+    # this if-else loop switches between words
+    if current_word == word:
+        current_count += count
+    else:
+        # write result to STDOUT
+        if current_word:
+            print('%s\t%s' % (current_word, current_count))
+        current_count = count
+        current_word = word
+
+# do not forget to output the last word if needed!
+if current_word == word:
+    print('%s\t%s' % (current_word, current_count))
